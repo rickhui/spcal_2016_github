b7f3714782c7739196988a3f10bcb21facb77110
lencialam <lencialam@gmail.com>
2017-03-01 10:59:13 +0800

    Create Collection Trial

--- spcal/collections/collections.js
+++ spcal/collections/collections.js
@@ -1,1 +1,21 @@
-dcdcData = new Mongo.Collection("dcdc");
+import jquery from 'meteor/jquery'
+import babyParse from 'babyparse'
+
+DcdcData = new Mongo.Collection("dcdc");
+/*
+Meteor.methods({
+  importFromCSV(data) {
+    for (let i = 0; i < data.length; i++) {
+      let item = data[i];
+      let exists = DcdcData.findOne({ })
+    }
+  }
+});
+
+$(document).ready(function () {
+  $.get('dcdcSource.csv', function(data) {
+    var dcdcJson = babyParse.parse(data);
+    console.log(dcdcJson);
+  });
+});
+*/
