b71c12aad910a3ba6dcc02bd1d264a4f7c1cc0a6
lencialam <lencialam@gmail.com>
2017-03-01 18:05:39 +0800

    Data import on startup bug fix

--- spcal/server/main.js
+++ spcal/server/main.js
@@ -7,7 +7,7 @@ Meteor.startup(() => {
   DcdcData = new Mongo.Collection("dcdc");
   var dcdcJson = JSON.parse(Assets.getText("dcdc.json"));
   dcdcJson.forEach(function (item) {
-    let exists = DcdcData.findOne({ underlying: item.underlying, strike: item.strike, ko_type: item.ko_type, ko_barrier: item.ko_barrier, coupon_pa: item.coupon_pa, note_price: item.note_price, tenor: item.tenor, barrier_type: item.barrier_type, ki_barrier: item.ki_barrier });
+    let exists = DcdcData.findOne({ underlying: item.underlying, strike: item.strike, ko_type: item.ko_type, ko_barrier: item.ko_barrier, coupon_pa: item.coupon_pa, tenor: item.tenor, barrier_type: item.barrier_type, ki_barrier: item.ki_barrier });
     if(!exists) {
       DcdcData.insert(item);
     }
