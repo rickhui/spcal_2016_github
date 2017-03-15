3722777798e0f3bf806269600f6baa025b8ea2d9
Shuya Lin <lencialam@gmail.com>
2017-03-15 11:00:10 +0800

    copy spcal to spcal_without_handson

---
+++ spcal_without_handson/server/main.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+
+  //Insert pricing data from dcdc
+  var dcdcJson = JSON.parse(Assets.getText("dcdc.json"));
+  dcdcJson.forEach(function (item) {
+    let exists = DcdcData.findOne({ underlying: item.underlying, strike: item.strike, ko_type: item.ko_type, ko_barrier: item.ko_barrier, coupon_pa: item.coupon_pa, tenor: item.tenor, barrier_type: item.barrier_type, ki_barrier: item.ki_barrier });
+    if(!exists) {
+      DcdcData.insert(item);
+    }
+  });
+
+});
